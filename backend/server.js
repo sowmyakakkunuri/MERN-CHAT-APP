78b21b8d576f74aa72fb9399d205f7354224cd7e
sowmyakakkunuri <sowmyakakkunuri7@gmail.com>
2024-05-26 17:58:12 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import { app, server } from "./socket/socket.js";
 
@@ -12,11 +13,9 @@ import friendRoutes from "./routes/friends.routes.js";
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
+const __dirname = path.resolve();
 
 app.use(express.json()); // to parse the incoming requests with json payloads
 app.use(cookieParser());
@@ -25,6 +24,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/friend", friendRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+});
 
 server.listen(port, () => {
   connectToMongoBD();
