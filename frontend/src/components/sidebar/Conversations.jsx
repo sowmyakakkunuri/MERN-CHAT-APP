02eb85d7878db1ac3c05d7e5fc45748e598a65f4
sowmyakakkunuri <sowmyakakkunuri7@gmail.com>
2024-05-28 18:31:00 +0530

    updated conversations page

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -6,13 +6,17 @@ const Conversations = () => {
   console.log("conversations", conversations);
   return (
     <div className="flex py-2 flex-col overflow-auto">
-      {conversations.map((conversation,idx) => (
+      {conversations.length === 0 && !loading && (
+        <div className="px-4 text-center sm-text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+          Click on the settings and add a friend to start a conversation!
+        </div>
+      )}
+      {conversations.map((conversation, idx) => (
         <Conversation
           key={conversation._id}
           conversation={conversation}
-          lastIdx={idx===conversations.length - 1}
+          lastIdx={idx === conversations.length - 1}
         />
-
       ))}
       {loading ? <span className="loading loading-spinner"></span> : null}
     </div>
